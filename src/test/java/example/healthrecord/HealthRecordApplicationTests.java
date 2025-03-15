a63dae9830c0d75919b9395e869b768e0526a718
parkJJae <wgpark0830@naver.com>
2025-03-16 02:47:26 +0900

    test

---
+++ src/test/java/example/healthrecord/HealthRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package example.healthrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
