203ea54cfcc2b6071813971298205e6e617cf323
parkJJae <wgpark0830@naver.com>
2025-03-16 02:47:57 +0900

    실행

---
+++ src/main/java/example/healthrecord/HealthRecordApplication.java
@@ -0,0 +1,13 @@
+package example.healthrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthRecordApplication.class, args);
+    }
+
+}
