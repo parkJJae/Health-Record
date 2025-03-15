425e6b5b660358467d37ad1c74d7855a89dde83f
parkJJae <wgpark0830@naver.com>
2025-03-16 02:50:02 +0900

    Entity

---
+++ src/main/java/example/healthrecord/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package example.healthrecord.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "userentity")
+public class UserEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String useremail;
+}
