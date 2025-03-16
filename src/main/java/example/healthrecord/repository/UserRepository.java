48da4ac224ff326ddd778d7fd7e1a80f6c7d1581
parkJJae <wgpark0830@naver.com>
2025-03-16 20:28:45 +0900

    레파지토리

--- src/main/java/example/healthrecord/repository/UserRepository.java
+++ src/main/java/example/healthrecord/repository/UserRepository.java
@@ -2,7 +2,9 @@ package example.healthrecord.repository;
 
 import example.healthrecord.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+        boolean existsByUserEmail(String userEmail);
 }
