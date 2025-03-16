4103e07b1b7ca1852a583050d73fa003b51d17b5
parkJJae <wgpark0830@naver.com>
2025-03-16 20:29:04 +0900

    Service

--- src/main/java/example/healthrecord/service/UserService.java
+++ src/main/java/example/healthrecord/service/UserService.java
@@ -3,6 +3,7 @@ package example.healthrecord.service;
 import example.healthrecord.dto.UserRequestDTO;
 import example.healthrecord.dto.UserResponseDTO;
 import example.healthrecord.entity.UserEntity;
+import example.healthrecord.exception.DuplecateEmailException;
 import example.healthrecord.repository.UserRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,9 @@ public class UserService {
 
     //클라이언트에서 요청한 DTO를 Entity로 바꾸는 것 .
     public UserEntity CreateUser(UserRequestDTO userrequestdto) {
+        if(userRepository.existsByUserEmail(userrequestdto.getUseremail())){
+            throw new DuplecateEmailException("이미 가입된 이메일입니다.");
+        }
         UserEntity user = new UserEntity();
         user.setUsername(userrequestdto.getUsername());
         user.setPassword(userrequestdto.getPassword());
